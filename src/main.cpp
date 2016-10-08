c05108f86912b056f450a43aab06ae21446c3dfc
arnaud <songac.arnaud@gmail.com>
2016-10-08 17:48:19 -0400

    Have the tests

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
 
+#include <cppunit/CompilerOutputter.h>
+#include <cppunit/extensions/TestFactoryRegistry.h>
+#include <cppunit/ui/text/TestRunner.h>
+
 #include "entity/Cell.h"
 #include "entity/Map.h"
 #include "service/MapRenderer.h"
@@ -9,6 +13,13 @@ using namespace std;
 
 int main()
 {
+    // Run tests prior to the execution of the driver
+    CppUnit::Test *suite = CppUnit::TestFactoryRegistry::getRegistry().makeTest();
+    CppUnit::TextUi::TestRunner runner;
+    runner.addTest(suite);
+    runner.setOutputter( new CppUnit::CompilerOutputter( &runner.result(), std::cerr ) );
+    bool wasSucessful = runner.run();
+
 
 
     cout << "************** Map Creator **************" << endl << endl;
