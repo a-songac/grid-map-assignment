53853b8f61b13de4a3603539d3a7952834ca87f1
arnaud <songac.arnaud@gmail.com>
2016-11-07 15:12:39 -0500

    Clean nik project

--- main.cpp
+++ main.cpp
@@ -2,10 +2,6 @@
 
 #include <iostream>
 
-#include <cppunit/CompilerOutputter.h>
-#include <cppunit/extensions/TestFactoryRegistry.h>
-#include <cppunit/ui/text/TestRunner.h>
-
 #include "entity/Cell.h"
 #include "entity/Map.h"
 #include "view/MapView.h"
@@ -18,19 +14,6 @@ using namespace std;
 
 int main()
 {
-
-#ifdef RUN_TESTS
-
-    CppUnit::Test *suite = CppUnit::TestFactoryRegistry::getRegistry().makeTest();
-    CppUnit::TextUi::TestRunner runner;
-    runner.addTest(suite);
-    runner.setOutputter( new CppUnit::CompilerOutputter( &runner.result(), std::cerr ) );
-    bool wasSucessful = runner.run();
-    if (!wasSucessful) {
-        return 0;
-    }
-
-#endif // RUN_TESTS
 
 
     EditorView::editorMenu();
