ff0e52a9b94f79201fb655d6ddd1c7f0c302c3ec
arnaud <songac.arnaud@gmail.com>
2016-11-09 12:55:32 -0500

    SAVE_POINT

--- core/Repository.h
+++ core/Repository.h
@@ -1,33 +1,40 @@
-//#pragma once
-//
-//#include "ProxyEntity.h"
-//#include "Repository.h"
-//#include <vector>
-//
-//
-//class Repository {
-//    public:
-//        static Repository* instance();
-//        ~Repository();
-//
-//
-//        std::vector<ProxyEntity<T*>*>* listAll();
-//        void save(ProxyEntity<T*>* entity);
-//        void save(string name);
-//        bool exists(string name);
-//        ProxyEntity<T*>* getEntity(string name);
-//        bool remove(string name);
-//        ProxyEntity<T*>* find(string name);
-//        T* loadEntity(string name);
-//
-//        virtual std::string getReferenceFileName() = 0;
-//
-//
-//    private:
-//        std::vector<ProxyEntity<T*>*>* _proxies;
-//        static Repository* _instance;
-//        Repository();
-//        void updateRepoReference();
-//        void loadGeneratedMaps();
-//
-//};
+#pragma once
+
+#include "Repository.h"
+#include <vector>
+
+template <typename T>
+class Repository {
+    public:
+        const static std::string MAP_REFERENCE;
+
+
+        static Repository* instance();
+        ~Repository();
+
+
+        std::vector<std::string>* listAll();
+        void save(std::string name, T entity);
+        bool exists(std::string name);
+        T* getEntity(std::string name);
+        bool remove(std::string name);
+        T* find(string name);
+
+        virtual std::string getReferenceFileName() = 0;
+
+    private:
+        std::vector<std::string>* _references;
+        T* _entities;
+
+        T* loadEntity(string name);
+        static Repository<T>* _instance;
+        Repository();
+        void updateRepoReference();
+        void loadGeneratedMaps();
+
+};
+
+template <class T>
+inline std::vector<std::string*>* Repository<T>::listAll() {
+    return this->_references;
+}
