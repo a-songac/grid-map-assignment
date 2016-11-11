5f11267da0f0fefaad8af55d0a7c3c2eab021622
arnaud <songac.arnaud@gmail.com>
2016-11-11 09:55:44 -0500

    implement abstrct template repository

--- core/Repository.cpp
+++ core/Repository.cpp
@@ -11,9 +11,10 @@
 
 using namespace std;
 
-//Repository<T*>* Repository::Repository;
-string Repository<T>::MAP_REFERENCE = "_map_references";
+template <class T>
+const int Repository<T>::REPO_CAPACITY = 20;
 
+//Repository<T*>* Repository::Repository;
 template <class T>
 Repository<T>* Repository<T>::instance() {
     if (!_instance) {
@@ -25,9 +26,8 @@ Repository<T>* Repository<T>::instance() {
 
 template <class T>
 Repository<T>::~Repository() {
-    delete _proxies;
+    delete _references;
 }
-
 
 
 ///  Load proxies of persisted map in the memory from _map_references
@@ -47,7 +47,7 @@ Repository<T>::Repository() {
     string entityName;
     T* entity;
 
-    ifstream in(ge);
+    ifstream in(this->getReferenceFileName());
     if (!in) {
         ofstream out;
         out.open(getReferenceFileName(), ios::trunc);
@@ -63,8 +63,12 @@ Repository<T>::Repository() {
                     "Constructor",
                     "FOR " + getReferenceFileName() + ": Could not create reference file.");
         #endif // DEBUG
-    }
 
+
+        _entities = new T[REPO_CAPACITY];
+
+    }
+    size_t i = 0;
     while (in >> entityName) {
         #ifdef DEBUG
             logInfo("Repository",
@@ -72,22 +76,106 @@ Repository<T>::Repository() {
                 "Load " + entityName);
         #endif // DEBUG
         this->_references->push_back(entityName);
+        this->_entities[i++] = nullptr;
     }
     in.close();
     #ifdef DEBUG
-        sStream << "FOR " << getReferenceFileName() << ": Total of " << this->_proxies->size() << " map references loaded.";
+        sStream << "FOR " << getReferenceFileName() << ": Total of " << this->_proxies->size() << " references loaded.";
         logInfo("Repository",
                 "Constructor",
                 sStream.str());
     #endif // DEBUG
 
     if (0 == this->_proxies->size()) {
-        loadGeneratedMaps();
 
         #ifdef DEBUG
             logInfo("Repository",
                     "Constructor",
-                    "FOR " + getReferenceFileName() + ": No existing maps, load and save the pregenerated maps");
+                    "FOR " + getReferenceFileName() + ": No existing entities, attempt to load the pregenerated entities if any...");
         #endif // DEBUG
+        this->loadGeneratedEntities();
+
     }
 }
+
+template <class T>
+int Repository<T>::getIndex(string name) {
+    for (size_t i = 0; i < this->_references->size(); i++) {
+        if (name == _references->at(i)) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+template <class T>
+bool Repository<T>::exists(string name) {
+
+    return this->getIndex(name) >= 0;
+}
+
+template <class T>
+void Repository<T>::updateRepoReference() {
+    ofstream out = loadOutputFileStream(this->getReferenceFileName());
+
+    for (size_t i = 0; i < this->_references->size(); i++) {
+        out << this->_references->at(i) << endl;
+    }
+    out.close();
+}
+
+
+template <class T>
+bool Repository<T>::save(string name, T* entity) {
+
+    int currentSize = this->_references->size();
+
+    if (!this-exists(name)) {
+        if (REPO_CAPACITY <= currentSize) {
+            #ifdef DEBUG
+                logInfo("Repository",
+                        "save",
+                        "FOR " + getReferenceFileName() + ": Cannot save new entity, reached max capacity of the repository");
+            #endif // DEBUG
+            return false;
+        }
+
+        this->_references->push_back(name);
+        this->_entities[currentSize] = entity;
+        this->updateRepoReference();
+    }
+    return this->persist(entity);
+}
+
+template <class T>
+T* Repository<T>::getEntity(std::string name) {
+    for (size_t i = 0; i < this->_references->size(); i++) {
+        if (name == this->_references->at(i)) {
+            return this->getEntity(i);
+        }
+    }
+    return nullptr;
+}
+
+template <class T>
+T* Repository<T>::getEntity(int index) {
+
+    T* entity;
+    string fileName = this->_references->at(index);
+
+    if (index < 0 || index > this->_references->size()) {
+        return nullptr;
+    }
+
+    entity = this->_entities[index];
+    if (nullptr == entity) {
+        #ifdef DEBUG
+            logInfo("Repository", "getEntity", "FOR " + getReferenceFileName() + ":Entity not yet loaded, load into memory");
+        #endif // DEBUG
+
+        this->_entities[index] = entity = this->loadEntity(fileName);
+
+    }
+    return entity;
+}
+
--- core/Repository.h
+++ core/Repository.h
@@ -1,40 +1,45 @@
 #pragma once
 
 #include "Repository.h"
+#include <string>
 #include <vector>
 
-template <typename T>
-class Repository {
+template <class T> class Repository {
     public:
-        const static std::string MAP_REFERENCE;
 
+        const static int REPO_CAPACITY;
 
         static Repository* instance();
         ~Repository();
 
 
         std::vector<std::string>* listAll();
-        void save(std::string name, T entity);
+        bool save(std::string name, T* entity);
         bool exists(std::string name);
         T* getEntity(std::string name);
+        T* getEntity(int index);
         bool remove(std::string name);
-        T* find(string name);
+        T* find(std::string name);
 
-        virtual std::string getReferenceFileName() = 0;
 
     private:
         std::vector<std::string>* _references;
         T* _entities;
 
-        T* loadEntity(string name);
+        virtual std::string getReferenceFileName() = 0;
+        virtual T* loadEntity(std::string name);
+
         static Repository<T>* _instance;
         Repository();
         void updateRepoReference();
-        void loadGeneratedMaps();
+        int getIndex(std::string name);
+
+        virtual void loadGeneratedEntities() = 0;
+        virtual bool persist(T* entity) = 0;
 
 };
 
 template <class T>
-inline std::vector<std::string*>* Repository<T>::listAll() {
+inline std::vector<std::string>* Repository<T>::listAll() {
     return this->_references;
 }
