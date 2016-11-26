14a88f10c89a44a0dc65e19bb0915c6c1d778356
cnoh93 <charbelnohra20@gmail.com>
2016-11-25 19:02:48 -0500

    small fix nothing serious

--- controller/GamePlayController.cpp
+++ controller/GamePlayController.cpp
@@ -1,4 +1,108 @@
+
 #include "GamePlayController.h"
+
+#include <iostream>
+#include <regex>
+#include <sstream>
+#include <string>
+#include <list>
+
+#include "../entity/Map.h"
+#include "../entity/Cell.h"
+#include "../entity/Character.h"
+#include "../entity/GamePlayer.h"
+#include "../entity/repo/MapRepository.h"
+#include "../utils/IOUtils.h"
+#include "../utils/LogUtils.h"
+#include "MapInteractionHelper.h"
+#include "../core/Repository.h"
+#include "../controller/CharacterEditorController.h"
+#include "../entity/repo/CharacterRepository.h"
+#include "../view/CharacterView.h"
+#include "../controller/CharacterEditorController.h"
+#include "../controller/ItemEditor.h"
+#include "CharacterInteractionHelper.h"
+#include "MapInteractionHelper.h"
+#include "../service/LogSettings.h"
+
+using namespace std;
+
+
+GamePlayController::GamePlayController() : level(1), map(nullptr) {}
+
+void GamePlayController::newGame() {
+
+	bool gameLoop = false;
+	cout << "\n************* New Game *************" << endl << endl;
+
+	do {
+
+		this->map = MapInteractionHelper::selectMap();
+		if (nullptr == this->map) {
+			return;
+		}
+		this->startGame();
+
+
+	} while (gameLoop);
+
+}
+
+
+void GamePlayController::startGame() {
+
+
+
+	string goTo, itemNameEquip, itemNameUnequip;
+	int input = 0;
+	bool quit = false;
+	Character* character;
+
+	character = CharacterInteractionHelper::selectCharacter();
+
+
+	cout << endl << endl << "********** GAME READY TO BE PLAYED**********" << endl;
+
+	bool startGame = readYesNoInput("Ready to start the game " + character->getName() + "?[Y/n]", 1);
+	if (!startGame) {
+		return;
+	}
+
+	if (nullptr != this->map) {
+
+		bool gameOver = false;
+
+		cout << "\n************* Start Game *************" << endl << endl;
+		Coordinate entryDoor = this->map->getEntryDoorCoordinate();
+
+
+		vector<GamePlayer*>* gamePlayers = this->map->getGamePlayers();
+		Coordinate* userLocation = new Coordinate(entryDoor.row, entryDoor.column);
+		GamePlayer userPlayer(character->getName(), userLocation, Cell::OCCUPANT_PLAYER);
+
+		this->map->movePlayer(entryDoor.row, entryDoor.column);
+		do {
+
+			gameOver = userPlayer.turn(this->map);
+
+			if (!gameOver) {
+				for (size_t i = 0; i < gamePlayers->size(); i++) {
+					gamePlayers->at(i)->turn(this->map);
+				}
+			}
+
+		} while (!gameOver);
+
+		// reset map to what it was
+		MapRepository::instance()->clearEntity(this->map->getName());
+
+	}
+	else {
+		logError("GamePlayController", "startGame", "No map was loaded for this game");
+	}
+
+}
+/*#include "GamePlayController.h"
 
 #include <iostream>
 #include <regex>
@@ -239,3 +343,4 @@ void GamePlayController::startGame() {
 
 }
 
+*/
--- entity/GamePlayer.cpp
+++ entity/GamePlayer.cpp
@@ -9,41 +9,46 @@
 #include "Cell.h"
 
 GamePlayer::GamePlayer(std::string elementReference, Coordinate* location, char type)
-: GameElement(elementReference, location), type(type)
+	: GameElement(elementReference, location), type(type)
 {
-    if (type == Cell::OCCUPANT_FRIEND) {
-        actionStrategy = new FriendlyPlayerStrategy();
-    } else if (type == Cell::OCCUPANT_OPPONENT) {
-        actionStrategy = new AggressivePlayerStrategy();
-    } else {
-        actionStrategy = new UserPlayerStrategy();
-    }
+	if (type == Cell::OCCUPANT_FRIEND) {
+		actionStrategy = new FriendlyPlayerStrategy();
+	}
+	else if (type == Cell::OCCUPANT_OPPONENT) {
+		actionStrategy = new AggressivePlayerStrategy();
+	}
+	else {
+		actionStrategy = new UserPlayerStrategy();
+	}
 }
 
 GamePlayer::GamePlayer(GamePlayer* gamePlayer)
-: GameElement(gamePlayer), type(gamePlayer->getType()) {
-    if (type == Cell::OCCUPANT_FRIEND) {
-        actionStrategy = new FriendlyPlayerStrategy();
-    } else if (type == Cell::OCCUPANT_OPPONENT) {
-        actionStrategy = new AggressivePlayerStrategy();
-    } else {
-        actionStrategy = new UserPlayerStrategy();
-    }
+	: GameElement(gamePlayer), type(gamePlayer->getType()) {
+	if (type == Cell::OCCUPANT_FRIEND) {
+		actionStrategy = new FriendlyPlayerStrategy();
+	}
+	else if (type == Cell::OCCUPANT_OPPONENT) {
+		actionStrategy = new AggressivePlayerStrategy();
+	}
+	else {
+		actionStrategy = new UserPlayerStrategy();
+	}
 }
 
 GamePlayer::GamePlayer() : type(Cell::OCCUPANT_EMPTY) {}
 
 
 bool GamePlayer::turn(Map* map) {
-    if (LOG::GAME) logInfo("GamePlayer", "turn", "Turn switching: " + getElementReference() + " - " + MapInteractionHelper::coordinateToString(getLocation()));
-    this->actionStrategy->turn(this, map);
+	if (LOG::GAME) logInfo("GamePlayer", "turn", "Turn switching: " + getElementReference() + " - " + MapInteractionHelper::coordinateToString(getLocation()));
+	this->actionStrategy->turn(this, map);
+	return true;
 
 }
 
 
 std::string GamePlayer::getTypeName() {
-    if (type == Cell::OCCUPANT_FRIEND) {
-        return "Friendly";
-    }
-    return "Hostile";
+	if (type == Cell::OCCUPANT_FRIEND) {
+		return "Friendly";
+	}
+	return "Hostile";
 }
